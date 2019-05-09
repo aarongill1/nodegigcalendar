5f4b4b4aa4d94b4cbff64b10136e401f2f3addd2
Aaron Gill <izzydiggler@gmail.com>
2019-05-09 22:06:37 +0100

    Users DB added and some other functionality - going to transition to postgres DB, so baselining

--- routes/index.js
+++ routes/index.js
@@ -1,46 +1,67 @@
 const express = require('express');
 const router = express.Router();
 const Joi = require('joi');
-const collection = require('../mongoUtil');
+const gigsCollection = require('../mongoGigsDB');
+const usersCollection = require('../mongoUsersDB');
 const Gig = require('../model/gig');
+const User = require('../model/user');
 const mongo = require('mongodb')
 
 router.use(express.static('public'));
 var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: true }));
 
-  const schema = Joi.object().keys({
-    artist: Joi.string().min(1).max(50).required(),
-    date: Joi.date().required()
+const gigsSchema = Joi.object().keys({
+  artist: Joi.string().min(1).max(50).required(),
+  date: Joi.date().required()
+});
+
+const usersSchema = Joi.object().keys({
+  username: Joi.string().min(1).max(50).required(),
+  password: Joi.required()
 });
 
 router.get('/gigbook', function(req, res) {
-  collection.gigs.find().toArray(function(err, docs) {
+  gigsCollection.gigs.find().toArray(function(err, docs) {
   res.render('index', { title: 'this is a test', gigList:docs });
   });
 });
 
-router.post('/gigbook/:user/addgig', function(req, res) {
-  const { error } = Joi.validate({ artist: req.body.artist, date: req.body.date }, schema);
+router.post('/gigbook/login', function(req, res) {
+  usersCollection.users.findOne( { _id: new mongo.ObjectID('5cd48b305cf715f9dee2f8df') });
+  res.redirect('/gigbook');
+  });
+
+router.get('/gigbook/newuser', function(req, res) {
+  console.log('test');
+  res.render('registration');
+});
+
+router.post('/gigbook/adduser', function(req, res) {
+  const { error } = Joi.validate({ username: req.body.registrationuser, password: req.body.registrationpassword }, usersSchema);
     if(error) {
-      // temp status code and error - need to add to front end
+      res.status(400).send(error);
+      return;
+    }
+  const entry = new User(req.body.registrationuser, req.body.registrationpassword);
+  usersCollection.users.insertOne(entry);
+  res.redirect('/gigbook');
+});
+
+router.post('/gigbook/:user/addgig', function(req, res) {
+  const { error } = Joi.validate({ artist: req.body.artist, date: req.body.date }, gigsSchema);
+    if(error) {
       res.status(400).send(error);
       return;
     }
     const entry = new Gig(req.body.artist, req.body.date);
-    collection.gigs.insertOne(entry);
+    gigsCollection.gigs.insertOne(entry);
     res.redirect('/gigbook');
 });
 
 router.post('/gigbook/delete', function(req, res) {
-  collection.gigs.deleteOne( { _id: new mongo.ObjectID(req.body._id) });
+  gigsCollection.gigs.deleteOne( { _id: new mongo.ObjectID(req.body._id) });
   res.redirect('/gigbook');
 });
 
 module.exports = router;
-
-//for login - future feature
-
-// router.get('/user/:name', function(req, res) {
-// res.render('index', { title: 'Account Options' });
-// });
